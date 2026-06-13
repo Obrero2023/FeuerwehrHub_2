572eb2e24d05db5c74a47c52f5ab2b2a4fa6908e
Christian <chris-hb@web.de>
2026-06-13 23:02:25 +0200

    debug

--- frontend/src/js/pages/vehicle_checklist.js
+++ frontend/src/js/pages/vehicle_checklist.js
@@ -3,6 +3,7 @@ import { renderShell, setShellInfo } from '../shell.js';
 import { icon, renderIcons } from '../icons.js';
 
 export async function rendervehicle_checklist() {
+    console.log('Fahrzeugprüfung geladen');
   const [settings, user] = await Promise.all([api.getSettings(), api.me()]);
   setShellInfo(settings?.ff_name, user, settings?.modules);
   renderShell('fahrzeugpruefung');
