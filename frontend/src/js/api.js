ae74e70a8c05cacdc2e99c1ec585834f3c2c57d1
Obrero2023 <138330581+Obrero2023@users.noreply.github.com>
2026-09-11 22:53:35 +0200

    Revert "module_intranet_4"

--- frontend/src/js/api.js
+++ frontend/src/js/api.js
@@ -429,7 +429,6 @@ export const api = {
       credentials: "include",
     });
   },
-  updateIntranetEntry: (id, body) => request("PUT", `/intranet/${id}`, body),
   deleteDocument: (id) => request("DELETE", `/verein/dokumente/${id}`),
   downloadDocument: (id) => {
     return fetch(`${BASE}/verein/dokumente/${id}/download`, {
